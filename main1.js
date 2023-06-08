d83f1dbcdd69d9a404d3b0591705780748c577c2
franco sassi <franco.sassi97@gmail.com>
2023-06-08 11:42:01 -0300

    turnos

--- main1.js
+++ main1.js
@@ -42,6 +42,8 @@ consulta.addEventListener("input", () => {
   crearHTML2(nuevoFiltro);
   })};
 
+
+
 const btnTurnos = document.getElementsByClassName('btnTurnos');
 const modalCalendar = document.getElementsByClassName('modalCalendar');
 
@@ -57,3 +59,52 @@ if(as){
 as.addEventListener('click',()=>{
   modalCalendar.classList.toggle('modal-active'); 
 } )}
+ 
+
+const btnSwal = document.getElementById("botonSwal"),
+  btnToast = document.getElementById("botonToast"),
+  horaActual = document.querySelector("#horaActual");
+ 
+//LUXON
+ const DateTime = luxon.DateTime;
+ 
+  const ahora= DateTime.now()
+  
+let dates = document.querySelectorAll('input[type="date"]');
+let inicio = DateTime.now().toFormat("yyyy-MM-dd");
+// let fin = DateTime.now().plus({ months: 4 }).toFormat("yyyy-MM-dd");
+ 
+dates.forEach((element) => {
+  element.setAttribute("min", inicio);
+  // element.setAttribute("max", fin);
+});
+
+// function calcularEstadia(checkIn, checkOut) {
+//   let total = checkOut.diff(checkIn);
+//   return total.as("days");
+// }
+
+// function calcularPrecioTotal(estadia, precio) {
+//   return estadia * precio;
+// }
+
+ const btnCalcular= document.querySelector("#calcular")
+btnCalcular.addEventListener("click", () => {
+  
+  let checkIn = DateTime.fromISO(document.getElementById("checkIn").value);
+  // let checkOut = DateTime.fromISO(document.getElementById("checkOut").value);
+   
+ 
+  Swal.fire({
+    title: "Turno Agendado!",
+    // text: `Tu estadía de ${estadia} días tiene un precio total de $${precioTotal} el dia ${checkIn.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)}`,
+    text: `Turno reservado el dia ${checkIn.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)} a las 17:00hs`,
+
+    icon: "info",
+    iconColor: "#00ff00",
+    showCancelButton: true,
+    confirmButtonText: "Confirmar",
+    cancelButtonText: "Cancelar",
+    position: "top-center",
+    backdrop: "#445566aa",
+  });});
